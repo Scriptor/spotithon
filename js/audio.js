f8858286dd58888e0c5dd498b5b2e9ac17cf755d
kermit <elforr@gmail.com>
2015-05-23 15:25:47 -0400

    makes a tone

--- js/audio.js
+++ js/audio.js
@@ -4,6 +4,8 @@ function init() {
     try {
         window.AudioContext = window.AudioContext||window.webkitAudioContext;
         context = new AudioContext();
+        var snd = new Osc()
+        snd.toggle();
     } catch(e) {
         alert('Web Audio API is not supported in this browser');
     }
@@ -17,11 +19,23 @@ Osc.prototype.play = function() {
     this.oscillator = context.createOscillator();
     this.gain = context.createGain();
     
+    this.oscillator.type = "triangle";
+    this.oscillator.frequency.value = 500;
+    this.gain.value = 0.2;
+    
     this.oscillator.connect(this.gain);
     this.gain.connect(context.destination);
-    
-    this.gain.value = 0.2;
     
     this.oscillator[this.oscillator.start ? 'start' : 'noteOn'](0);
     
 }
+
+Osc.prototype.stop = function() {
+    this.oscillator.stop(0);
+}
+
+Osc.prototype.toggle = function() {
+  (this.isPlaying ? this.stop() : this.play());
+  this.isPlaying = !this.isPlaying;
+
+};
